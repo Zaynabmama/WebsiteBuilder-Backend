582c1faf48caba2dac58cd1cc75709052bff3c88
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 02:54:01 +0300

    Configuring application port through .env file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Nest application is starting...');
-  await app.listen(3000);
+  await app.listen(process.env.PORT , () => {
+    console.log(`Application is running on: http://localhost:${process.env.PORT }`);
+  });
 }
 bootstrap();
