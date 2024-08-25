a328b1c4e81210d987c6e5e1e7f953666dbd9b2b
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 16:40:20 +0300

    Injecting UserModel into UserService constructor

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,5 +8,5 @@ import { User } from './schemas/user.schema/user.schema';
 @Injectable()
 // provider :it can be injected in def place  as dependencies
 export class UserService {
-
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 }
