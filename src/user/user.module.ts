e58d5e30b0d629d46add88625ffa1e64b13ff3dd
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 18:30:38 +0300

    Fixed UserModule configuration

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './schemas/user.schema/user.schema';
+import { User, UserSchema } from './schemas/user.schema/user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name:'user',schema: UserSchema}]),// Registering the UserSchema with Mongoose
+  imports: [MongooseModule.forFeature([{name:User.name,schema: UserSchema}]),// Registering the UserSchema with Mongoose
 ],
 
 
