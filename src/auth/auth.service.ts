b3b31f83a37d761fb1b731872d494932c6f49103
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 02:28:23 +0300

    Implementing validateUser method in AuthService to validate user credentials

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,4 +6,15 @@ import { UserService } from 'src/user/user.service';
 export class AuthService {
   constructor(private readonly userService: UserService) {} 
 
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username); 
+    if (user && user.password === pass) {//user exist and password matche
+      const { password, ...result } = user; // Remove password 
+      return result; //without the password
+    }
+    return null; //validation failed
+  }
+
+
 }
