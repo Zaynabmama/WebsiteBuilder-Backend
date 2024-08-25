de2b9010109d7f77216ef12f6cd7523d6ba10d26
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 02:45:57 +0300

    Implementing JWT token generation in login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,5 +6,10 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {}
     @Post('login')
     async login(@Body() body: { name: string; password: string }) {
+        const user = await this.authService.validateUser(body.name, body.password);
+        if (!user) {
+            return { message: 'Invalid credentials' };
+        }
+        return this.authService.login(user); 
   }
-}
+  }
