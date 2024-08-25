4b91831b510481ab3e9a36c0865e636bdde8f4e7
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 15:40:38 +0300

    Configuring UserModule with Mongoose and UserSchema

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema/user.schema';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [MongooseModule.forFeature([{name:'user',schema: UserSchema}]),// Registering the UserSchema with Mongoose
+],
+
+
+  controllers: [UserController],//api endpoints
+  providers: [UserService],//business logic for users
 })
 export class UserModule {}
