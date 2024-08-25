db9daf53c895f67863015b528f2b7e4e46476150
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 21:13:43 +0300

    Applying HttpExceptionFilter globally

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Nest application is starting...');
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe());
   await app.listen(process.env.PORT , () => {
