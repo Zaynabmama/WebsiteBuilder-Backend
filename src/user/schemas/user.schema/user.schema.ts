1390342d553c803b1c1c076931c4a3f3a442549f
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 12:19:41 +0300

    Initialized User schema with name

--- src/user/schemas/user.schema/user.schema.ts
+++ src/user/schemas/user.schema/user.schema.ts
@@ -1,1 +1,9 @@
-export class UserSchema {}
+import { Prop ,Schema, SchemaFactory, } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class User extends Document {
+    @Prop({required:true })
+    name: string;
+}
+export const UserSchema =SchemaFactory.createForClass(User);
