41272de9ace883a3e34520037e51d70b0caa0d59
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-25 18:54:35 +0300

    Adding global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import * as dotenv from 'dotenv';
 dotenv.config(); 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Nest application is starting...');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT , () => {
     console.log(`Application is running on: http://localhost:${process.env.PORT }`);
   });
