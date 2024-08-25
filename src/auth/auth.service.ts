2620d052471c30602137817c139d22a104435a2e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 02:30:48 +0300

    Injecting JwtService into AuthService for JWT token handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {} 
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+    ) {} 
 
 
   async validateUser(username: string, pass: string): Promise<any> {
