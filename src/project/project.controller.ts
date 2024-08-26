6fcd5f5e76a05c78fac207064190b0b14e5d0d1d
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 22:02:19 +0300

    Adding CreateProjectDto to validate project creation data

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('project')
@@ -8,7 +9,7 @@ export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
   
   @Post()
-  async createProject(@Req() req) {
+  async createProject(@Req() req , @Body() createProjectDto: CreateProjectDto) {
     const userId = req.user.userId; //extract userId from auth request
   }
 }
