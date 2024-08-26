3091ffa110c4af058b8d45babe65c7037b5988b2
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 19:41:24 +0300

    injecting the Mongoose model associated with the User schema

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/user/schemas/user.schema/user.schema';
+
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(
+    
+    @InjectModel(User.name) private userModel: Model<User>,// injects the Mongoose model associated with the User schema to interact with the User collection in MongoDB
+  ) {}
+
+}
