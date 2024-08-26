4ecadd26c8a05fe2d85054a8c37625bfdc200302
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 16:02:09 +0300

    implementing AuthService with validateUser and login methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,15 +17,22 @@ export class AuthService {
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(email); 
     if (user && await bcrypt.compare(pass, user.password)) {
-        const { password, ...result } = user;
+        const { password, ...result } = user.toObject();
         return result;
       }
       throw new BadRequestException('Invalid credentials');
     }
    
-      async login(user: any) {
-        const payload = { email: user.email, sub: user.userId }; 
-        console.log('JWT Secret in AuthService:', this.jwtService['options']?.secret);  // Log the secret being used
+    //   async login(user: any) {
+    //     const payload = { email: user.email, sub: user.userId ,role: user.role }; 
+    //     console.log('JWT Secret in AuthService:', this.jwtService['options']?.secret); 
+    //     return {
+    //       access_token: this.jwtService.sign(payload),
+    //     };
+    //   }
+    async login(user: any) {
+        const payload = { email: user.email, sub: user._id, role: user.role };
+        console.log('JWT Payload:', payload);
         return {
           access_token: this.jwtService.sign(payload),
         };
