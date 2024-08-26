6c891cf7c2459ad5ef32c207b58d97098322c095
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-26 21:58:24 +0300

    adding JWT guard to protect routes

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 import { ProjectService } from './project.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('project')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
+  
+  @Post()
+  async createProject() {
+   
+  }
 }
