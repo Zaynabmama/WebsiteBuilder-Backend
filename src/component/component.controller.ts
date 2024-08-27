f722953da3b2b156f644328c23d2912ab2b7af0e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 02:13:57 +0300

    Adding addComponent method to the controller

--- src/component/component.controller.ts
+++ src/component/component.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { ComponentService } from './component.service';
+import { CreateComponentDto } from './dto/create-component.dto';
 
 @Controller('userId/:projectId/:pageId/components')
 export class ComponentController {
     constructor(private readonly componentService: ComponentService) {}
+
+    @Post()
+    async addComponent(
+        @Param('userId') userId: string,
+        @Param('projectId') projectId: string,
+        @Param('pageId') pageId: string,
+        @Body() createComponentDto: CreateComponentDto,
+    ): Promise<any> {}
 
 }
