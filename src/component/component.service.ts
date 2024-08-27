0f521df3fda3e64fe5854639576bcc8d77eea6f6
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 01:24:32 +0300

    injecting the Mongoose model associated with the User schem

--- src/component/component.service.ts
+++ src/component/component.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ComponentService {}
+export class ComponentService {
+constructor(
+    @InjectModel(User.name) private userModel: Model<User>,// injects the Mongoose model associated with the User schema to interact with the User collection in MongoDB
+  ) {}
+}
