3a325c14e3e5b4665d6b52421638cdf7d9ad6425
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 02:20:07 +0300

    importing userModule

--- src/component/component.module.ts
+++ src/component/component.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ComponentService } from './component.service';
 import { ComponentController } from './component.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports:[UserModule],
   providers: [ComponentService],
   controllers: [ComponentController]
 })
