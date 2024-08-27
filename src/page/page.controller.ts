14d87e5b3d644d40dea361f4c6a66d85df7d5b0b
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-27 12:11:59 +0300

    Injecting PageService into the controller

--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ProjectService } from 'src/project/project.service';
 
 @Controller('page')
-export class PageController {}
+export class PageController {
+    constructor(private readonly projectService: ProjectService) {}
+}
