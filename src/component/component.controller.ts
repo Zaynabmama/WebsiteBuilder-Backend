47a4a4fc57b43f7e70535a06a207faa3114f8db6
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 02:11:25 +0300

    Injecting component service into the controller

--- src/component/component.controller.ts
+++ src/component/component.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { ComponentService } from './component.service';
 
 @Controller('userId/:projectId/:pageId/components')
-export class ComponentController {}
+export class ComponentController {
+    constructor(private readonly componentService: ComponentService) {}
+
+}
