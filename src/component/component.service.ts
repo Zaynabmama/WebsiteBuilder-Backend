c79f09a1225db2fa26bbd633d6dcb1646ef51a14
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 01:47:49 +0300

    Adding  addComponent method in the service

--- src/component/component.service.ts
+++ src/component/component.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
+import { CreateComponentDto } from './dto/create-component.dto';
 
 @Injectable()
 export class ComponentService {
 constructor(
     @InjectModel(User.name) private userModel: Model<User>,// injects the Mongoose model associated with the User schema to interact with the User collection in MongoDB
   ) {}
+  async addComponent(userId: string, createComponentDto: CreateComponentDto): Promise<any> {
+}
 }
