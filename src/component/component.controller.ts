bc0dc6e43c8f2bcdd9f1b78668c084a54a4b7af8
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 02:23:21 +0300

    Finialize addComponent method

--- src/component/component.controller.ts
+++ src/component/component.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Req } from '@nestjs/common';
 import { ComponentService } from './component.service';
 import { CreateComponentDto } from './dto/create-component.dto';
 
-@Controller('userId/:projectId/:pageId/components')
+@Controller(':projectId/:pageId/components')
 export class ComponentController {
     constructor(private readonly componentService: ComponentService) {}
 
     @Post()
     async addComponent(
-        @Param('userId') userId: string,
+        @Req() req,
         @Param('projectId') projectId: string,
         @Param('pageId') pageId: string,
         @Body() createComponentDto: CreateComponentDto,
-    ): Promise<any> {}
+    ): Promise<any> {
+        const userId = req.user.userId;
+        return this.componentService.addComponent(userId, projectId, pageId, createComponentDto);
+    }
 
 }
