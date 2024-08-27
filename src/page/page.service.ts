1dd7121d5d0b64f8a73987f8dcacf13cd9f1ebb1
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 00:21:21 +0300

    Adding deletePage method to pageservice

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -42,4 +42,25 @@ export class PageService {
     
         return project.pages;
       }
+
+      async deletePage(userId: string, projectId: string, pageId: string): Promise<any> {
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+          throw new NotFoundException('User not found');
+        }
+    
+        const project = user.projects.id(projectId);
+        if (!project) {
+          throw new NotFoundException('Project not found');
+        }
+    
+        const page = project.pages.id(pageId);
+        if (!page) {
+          throw new NotFoundException('Page not found');
+        }
+    
+        project.pages.pull(pageId);
+        await user.save();
+    
+      }
 }
