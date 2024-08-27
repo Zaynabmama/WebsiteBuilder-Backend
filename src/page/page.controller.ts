14dd61a7ba42876a34303bb667dfdee545158219
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-27 12:36:33 +0300

    adding JWT guard to protect routes

--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { PageService } from './page.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('page')
 export class PageController {
     constructor(private readonly pageService: PageService) {}
