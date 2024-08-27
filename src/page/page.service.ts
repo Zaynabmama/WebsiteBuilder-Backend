5880ca698ea0d02cb7f589e4b16926e45060aa2c
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-27 12:03:35 +0300

    Finding the user by their ID

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
+import { CreatePageDto } from './dto/create-page.dto';
 
 @Injectable()
 export class PageService {
@@ -10,6 +11,9 @@ export class PageService {
         @InjectModel(User.name) private userModel: Model<User>,
       ) {}
       async createPage(userId: string, projectId: string, createPageDto: CreatePageDto): Promise<any> {
+        const user = await this.userModel.findById(userId);
         
+    
+    
       }
 }
