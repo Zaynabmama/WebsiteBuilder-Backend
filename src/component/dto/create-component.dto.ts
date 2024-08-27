e1ea92808f8c1304fd0199fd7b0ac8860c399001
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 02:30:18 +0300

    Adding dto with dome validation

--- src/component/dto/create-component.dto.ts
+++ src/component/dto/create-component.dto.ts
@@ -1,3 +1,12 @@
+import { IsString, IsOptional } from 'class-validator';
 export class CreateComponentDto {
+@IsString()
+    readonly name: string;
+
+    @IsString()
+    readonly type: string;
+
+    @IsOptional()
+    readonly properties?: any;
 
   }
