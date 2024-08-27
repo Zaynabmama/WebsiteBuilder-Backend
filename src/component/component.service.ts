3af093a81477362391e3c2aad0402f56c06a8a10
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 01:49:26 +0300

    Adding error handling for user not found in addComponent method

--- src/component/component.service.ts
+++ src/component/component.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
@@ -11,6 +11,9 @@ constructor(
   ) {}
   async addComponent(userId: string, createComponentDto: CreateComponentDto): Promise<any> {
     const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+  }
 
 }
 }
