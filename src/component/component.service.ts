6e6e7737f633bbc19126be171387fef7b33e640e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 01:48:29 +0300

    Retrieving user by ID in addComponent method

--- src/component/component.service.ts
+++ src/component/component.service.ts
@@ -10,5 +10,7 @@ constructor(
     @InjectModel(User.name) private userModel: Model<User>,// injects the Mongoose model associated with the User schema to interact with the User collection in MongoDB
   ) {}
   async addComponent(userId: string, createComponentDto: CreateComponentDto): Promise<any> {
+    const user = await this.userModel.findById(userId);
+
 }
 }
