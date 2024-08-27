4dccdc30724af748874ec796f96c9c8bf7a150d1
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-27 12:52:02 +0300

    Adding CreatePageDto to validate page creation data

--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { PageService } from './page.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CreatePageDto } from './dto/create-page.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('page')
@@ -9,7 +10,7 @@ export class PageController {
 
 
     @Post()
-    async createProject(@Req() req) {
+    async createProject(@Req() req  ,projectId: string, createPageDto: CreatePageDto) {
     const userId = req.user.userId; //extract userId from auth request
   }
 }
