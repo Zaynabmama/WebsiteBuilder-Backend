ba899e5dc00ab79435e39a1f6103a51bdd4c6eb7
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-27 23:21:49 +0300

    adding params and body

--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { PageService } from './page.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreatePageDto } from './dto/create-page.dto';
 
 @UseGuards(JwtAuthGuard)
-@Controller('page')
+@Controller('project/:projectId/page')
 export class PageController {
     constructor(private readonly pageService: PageService) {}
 
 
     @Post()
-    async createPage(@Req() req  ,projectId: string, createPageDto: CreatePageDto) {
+    async createPage(@Req() req  ,@Param('projectId') projectId: string, @Body()createPageDto: CreatePageDto) {
     const userId = req.user.userId; //extract userId from auth request
     return this.pageService.createPage(userId, projectId, createPageDto);
   
