4871e5370d7f6437737542d8cf469dd38d1f5bb9
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-29 00:54:18 +0300

    updating create page

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -10,35 +10,70 @@ export class PageService {
     constructor(
 
         @InjectModel(User.name) private userModel: Model<User>,
-         private readonly fileService: FileService
+         private readonly fileService: FileService,
+        // private readonly componentService: ComponentService,
       ) {}
-      async createPage(userId: string, projectId: string, createPageDto: CreatePageDto): Promise<any> {
-        const user = await this.userModel.findById(userId);
-        if (!user) {
-            throw new NotFoundException('User not found');
-          }
-        const project = user.projects.id(projectId);
-        //const jsxFilePath = `src/projects/${projectId}/${createPageDto.name}.jsx`;
+  //     async createPage(userId: string, projectId: string, createPageDto: CreatePageDto): Promise<any> {
+  //       const user = await this.userModel.findById(userId);
+  //       if (!user) {
+  //           throw new NotFoundException('User not found');
+  //         }
+  //       const project = user.projects.id(projectId);
+  //       //const jsxFilePath = `src/projects/${projectId}/${createPageDto.name}.jsx`;
     
-        if (!project) {
-            throw new NotFoundException('Project not found');
-        }
-        //const newPage=user.projects.push(createPageDto as any);
-        const jsxFilename = `page-${createPageDto.name}.jsx`;
-        const newPage = project.pages.create({
-          ...createPageDto,
-          jsxFilePath: jsxFilename, // Set the jsxFilePath here
-        });
-        project.pages.push(newPage);
-        const jsxContent = this.generateInitialJsxContent();
+  //       if (!project) {
+  //           throw new NotFoundException('Project not found');
+  //       }
+  //       //const newPage=user.projects.push(createPageDto as any);
+  //       const jsxFilename = `page-${createPageDto.name}.jsx`;
+  //       const newPage = project.pages.create({
+  //         ...createPageDto,
+  //         jsxFilePath: jsxFilename,
+  //       });
+  //       project.pages.push(newPage);
+  //       const jsxContent = this.generateInitialJsxContent();
         
-        await this.fileService.uploadJsxFile(jsxContent, jsxFilename); // Save JSX content to a file
-        await user.save();
-        return newPage
-      }
-  generateInitialJsxContent() : string  {
-    return `<div>\n</div>`;
-  }
+  //       await this.fileService.uploadJsxFile(jsxContent, jsxFilename);
+  //       await user.save();
+  //       return newPage
+  //     }
+  // generateInitialJsxContent() : string  {
+  //   return `<div>\n</div>`;
+  // }
+  async createPage(userId: string, projectId: string, createPageDto: CreatePageDto): Promise<any> {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+        throw new NotFoundException('User not found');
+    }
+
+    const project = user.projects.id(projectId);
+    if (!project) {
+        throw new NotFoundException('Project not found');
+    }
+
+    const projectDir = project.name.replace(/\s+/g, '-').toLowerCase();
+    const pageDir = createPageDto.name.replace(/\s+/g, '-').toLowerCase();
+    const jsxFilePath = `${projectDir}/${pageDir}.jsx`;
+
+    
+    const newPage = project.pages.create({
+        ...createPageDto,
+        jsxFilePath,
+    });
+    project.pages.push(newPage);
+
+    const jsxContent = this.generateInitialJsxContent();
+
+    await this.fileService.uploadJsxFile(jsxContent, jsxFilePath);
+    await user.save();
+
+    
+    return newPage;
+}
+
+    generateInitialJsxContent(): string {
+    return `<div>\n</div>`;}
+
       async listPages(userId: string, projectId: string): Promise<any[]> {
         const user = await this.userModel.findById(userId);
         if (!user) {
@@ -71,7 +106,8 @@ export class PageService {
     
         project.pages.pull(pageId);
         await user.save();
-    
+      // Delete the JSX file
+      await this.fileService.deleteJsxFile(page.jsxFilePath);
       }
       async getPageById(userId: string, projectId: string, pageId:string): Promise<any> {
         const user = await this.userModel.findById(userId);
@@ -90,5 +126,15 @@ export class PageService {
           return page;
         
         
-      }
+      // }
+      // async updatePage(pageId: string, updatePageDto: CreatePageDto): Promise<any> {
+      //   const page = await this.userModel.findById(pageId);
+      //   if (!page) {
+      //     throw new NotFoundException('Page not found');
+      //   }
+      //   Object.assign(page, updatePageDto);
+      //   await page.save();
+      //   await this.componentService.updateJsxFile(page._id); // Update JSX file
+      //   return page;
+        }
 }
