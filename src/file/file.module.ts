00479e49eccf0cfad81b2fbdd3d78797515d6e31
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 20:27:58 +0300

    Registering Multer with the configuration provided by FileService

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+    imports: [
+        MulterModule.registerAsync({
+          useFactory: (fileService: FileService) => ({
+            storage: fileService.getMulterStorage(),
+          }),
+        }),
+      ],
   controllers: [FileController],
   providers: [FileService]
 })
