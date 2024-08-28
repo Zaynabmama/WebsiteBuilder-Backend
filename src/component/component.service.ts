ad5357fdfce7f1f85cd91d9d2c78d2a7d81d9010
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 03:07:40 +0300

    Adding updateComponent method to service

--- src/component/component.service.ts
+++ src/component/component.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
 import { CreateComponentDto } from './dto/create-component.dto';
+import {UpdateComponentDto} from './dto/update-component.dto';
 
 @Injectable()
 export class ComponentService {
@@ -55,5 +56,32 @@ constructor(
 
     return { message: 'Component deleted successfully' };
 }
+async updateComponent(userId: string, projectId: string, pageId: string, componentId: string, updateComponentDto: UpdateComponentDto): Promise<any> {
+  const user = await this.userModel.findById(userId);
+  if (!user) {
+      throw new NotFoundException('User not found');
+  }
+
+  const project = user.projects.id(projectId);
+  if (!project) {
+      throw new NotFoundException('Project not found');
+  }
+
+  const page = project.pages.id(pageId);
+  if (!page) {
+      throw new NotFoundException('Page not found');
+  }
+
+  const component = page.components.id(componentId);
+  if (!component) {
+      throw new NotFoundException('Component not found');
+  }
+
+  Object.assign(component, updateComponentDto);
+  await user.save();
+
+  return component;
+}
+
 
 }
