262436a973723e7e9920951e95069e8c1ff84eff
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 21:02:50 +0300

    Injecting FileService

--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -3,12 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
 import { CreatePageDto } from './dto/create-page.dto';
+import { FileService } from 'src/file/file.service';
 
 @Injectable()
 export class PageService {
     constructor(
 
         @InjectModel(User.name) private userModel: Model<User>,
+        private readonly fileService: FileService,
       ) {}
       async createPage(userId: string, projectId: string, createPageDto: CreatePageDto): Promise<any> {
         const user = await this.userModel.findById(userId);
