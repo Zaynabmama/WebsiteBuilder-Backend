4c1aca09c93eba0b349484c49e7e33316368bb64
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 03:08:05 +0300

    Fixing error

--- src/component/dto/update-component.dto.ts
+++ src/component/dto/update-component.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateComponentDto } from './create-component.dto';
 
-export class UpdateUserDto extends PartialType(CreateComponentDto) {}
+export class UpdateComponentDto extends PartialType(CreateComponentDto) {}
