af4a2259e5788e033bcd57a3a7c2f295e145366e
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 21:50:32 +0300

    fixing issues in pagemodule

--- src/page/page.module.ts
+++ src/page/page.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PageService } from './page.service';
 import { PageController } from './page.controller';
+import { UserModule } from 'src/user/user.module';
+import { FileModule } from 'src/file/file.module';
 
 
 
 
 @Module({
+  imports:[
+    UserModule ,
+    FileModule
+  ],
   providers: [PageService],
   controllers: [PageController]
 })
