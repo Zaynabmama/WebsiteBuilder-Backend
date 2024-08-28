0d0fb02430dd260188aac3915d4c4650a6c4dfb0
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-29 00:57:08 +0300

    "Updated ComponentController, ComponentModule, ComponentService, FileService, and PageModule with changes to method signatures, added exports, and modified file upload and deletion logic."

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,31 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { writeFile } from 'fs/promises';
-import { diskStorage } from 'multer';
-import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
 import * as path from 'path';
-
 
 @Injectable()
 export class FileService {
-  getMulterStorage() {
-    return diskStorage({
-      // Setting the destination for uploaded files
-      destination: (req, file, cb) => {
-        const uploadPath = path.join(__dirname, '..', '..', 'uploads');
-        cb(null, uploadPath); // the destination path
-      },
-      // Configuring the filename of the uploaded file
-      filename: (req, file, cb) => {
-        const fileExtension = path.extname(file.originalname); // Get the file extension
-        const filename = `${uuidv4()}${fileExtension}`; // Generate a unique filename using UUID
-        cb(null, filename); // the final filename
-      },
-    });
-  }
+  private baseUploadPath = path.resolve(__dirname, '..', '..', 'uploads');
 
-  async uploadJsxFile(content: string, filename: string): Promise<void> {
-    const filePath = path.join(__dirname, '..', '..', 'uploads', filename);
-    await writeFile(filePath, content, 'utf8');
-  }
+  async uploadJsxFile(content: string, filePath: string): Promise<void> {
+    const fullPath = path.join(this.baseUploadPath, filePath);
+    await fs.promises.mkdir(path.dirname(fullPath), { recursive: true });
+    await fs.promises.writeFile(fullPath, content, 'utf8');
+}
 
+  async deleteJsxFile(filePath: string): Promise<void> {
+    const fullPath = path.join(this.baseUploadPath, filePath);
+    if (fs.existsSync(fullPath)) {
+      fs.unlinkSync(fullPath);
+    }
+  }
 }
