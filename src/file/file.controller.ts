e49286926e5388d3fbbcd58301e64bf9be3126c7
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-28 20:35:04 +0300

    Adding endpoint that handles file uploads using the FileInterceptor provided by Multer

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileService } from './file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('file')
 export class FileController {
     constructor(private readonly fileService: FileService) {}
+    
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file')) //file:form field that contains the file
+    
+    uploadFile(@UploadedFile() file: Express.Multer.File) {
+    }
+    
 }
