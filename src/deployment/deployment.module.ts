f99c6c7df24e5033013e42fdd0f3f8eef6ab2990
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-30 02:03:58 +0300

    Adding DeploymentController to DeploymentModule controllers

--- src/deployment/deployment.module.ts
+++ src/deployment/deployment.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DeploymentService } from './deployment.service';
+import { DeploymentController } from './deployment.controller';
 
 @Module({
-  providers: [DeploymentService]
+  providers: [DeploymentService],
+  controllers: [DeploymentController]
 })
 export class DeploymentModule {}
