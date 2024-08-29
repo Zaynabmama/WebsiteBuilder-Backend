1874c9747c384184688bfcf88bc5f6368d920cc5
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-30 02:09:36 +0300

    Adding method for createSiteForUser in DeploymentService

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DeploymentService {}
+export class DeploymentService {
+    async createSiteForUser(userId: string, projectId: string): Promise<any> {
+      }
+}
