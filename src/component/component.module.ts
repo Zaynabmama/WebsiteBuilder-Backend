70c7a8d35bd41c7be67492e258ee2e397c98eb6c
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-29 10:32:27 +0300

    importing FileModule

--- src/component/component.module.ts
+++ src/component/component.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ComponentService } from './component.service';
 import { ComponentController } from './component.controller';
 import { UserModule } from 'src/user/user.module';
+import { FileModule } from 'src/file/file.module';
 
 @Module({
-  imports:[UserModule],
+  imports:[UserModule,FileModule],
   providers: [ComponentService],
   controllers: [ComponentController],
   exports:[ComponentModule]
