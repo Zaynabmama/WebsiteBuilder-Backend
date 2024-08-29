779bdc57a4c539b19eacdfc5be6cc5b9c250e4a0
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-30 02:20:16 +0300

    Using ConfigService in DeploymentService

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class DeploymentService {
+    constructor(private configService: ConfigService) {}
     async createSiteForUser(userId: string, projectId: string): Promise<any> {
+        const apiUrl = this.configService.get<string>('NETLIFY_API_URL');
+        const apiKey = this.configService.get<string>('NETLIFY_API_KEY');
       }
 }
