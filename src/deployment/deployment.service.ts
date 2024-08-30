8b75b3ac68b88f76af461f56ff6238ee905d59c9
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-30 03:15:07 +0300

    Fing user and project Id

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -9,11 +9,24 @@ import { User } from 'src/user/schemas/user.schema/user.schema';
 export class DeploymentService {
     @InjectModel(User.name) private userModel: Model<User> 
     constructor(private configService: ConfigService) {}
+
+    
     async createSiteForUser(userId: string, projectId: string): Promise<any> {
         const apiUrl = this.configService.get<string>('NETLIFY_API_URL');
         const apiKey = this.configService.get<string>('NETLIFY_API_KEY');
         console.log(`Netlify API URL: ${apiUrl}`);
         console.log(`Netlify API Key: ${apiKey}`);
+
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+              throw new NotFoundException('User not found');
+            }
+
+        const project = user.projects.id(projectId);
+        if (!project) {
+            throw new NotFoundException('Project not found');
+          }
+    
 
         const payload = {
         name: `user-site-${userId}-${projectId}`,
