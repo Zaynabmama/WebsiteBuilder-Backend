424432e2e9ea9bb3c86093694e1e227f39262205
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-30 03:18:44 +0300

    Implementing saveSiteIdToDeployment method to store siteId in Project

--- src/deployment/deployment.service.ts
+++ src/deployment/deployment.service.ts
@@ -10,24 +10,14 @@ export class DeploymentService {
     @InjectModel(User.name) private userModel: Model<User> 
     constructor(private configService: ConfigService) {}
 
-    
-    async createSiteForUser(userId: string, projectId: string): Promise<any> {
+
+    async createSiteForUser(userId: string, projectId: string): Promise<string> {
         const apiUrl = this.configService.get<string>('NETLIFY_API_URL');
         const apiKey = this.configService.get<string>('NETLIFY_API_KEY');
         console.log(`Netlify API URL: ${apiUrl}`);
         console.log(`Netlify API Key: ${apiKey}`);
 
-        const user = await this.userModel.findById(userId);
-        if (!user) {
-              throw new NotFoundException('User not found');
-            }
-
-        const project = user.projects.id(projectId);
-        if (!project) {
-            throw new NotFoundException('Project not found');
-          }
-    
-
+       
         const payload = {
         name: `user-site-${userId}-${projectId}`,
         };
@@ -40,5 +30,19 @@ export class DeploymentService {
         console.log(`Site created with ID: ${siteId}`);
           
         return siteId;  
+      }
+      async saveSiteIdToDeployment(userId: string, projectId: string, siteId: string): Promise<void> {
+        const user = await this.userModel.findById(userId);
+        if (!user) {
+          throw new NotFoundException('User not found');
+        }
+    
+        const project = user.projects.id(projectId);
+        if (!project) {
+          throw new NotFoundException('Project not found');
+        }
+    
+        project.deployment = { siteId, status: 'created', url: '' };
+        await user.save();
       }
 }
