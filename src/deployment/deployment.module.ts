b3314b044dfeb68981e0bdb11173bd15d18275ec
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-30 03:05:33 +0300

    Adding UserModule import to DeploymentModule

--- src/deployment/deployment.module.ts
+++ src/deployment/deployment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeploymentService } from './deployment.service';
 import { DeploymentController } from './deployment.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports:[UserModule],
   providers: [DeploymentService],
   controllers: [DeploymentController]
 })
