21adbfaf841ea2075c9094e437cd695281562b33
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-04 00:51:08 +0300

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  // Create the Nest application instance
   const app = await NestFactory.create(AppModule);
   console.log('Nest application is starting...');
-  
-  // Enable CORS for the frontend URL
+
   app.enableCors({
-    origin: 'http://localhost:3000', // The frontend URL
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies to be sent across domains
+    credentials: true,
   });
 
-  // Use global validation pipes for request validation
   app.useGlobalPipes(new ValidationPipe());
 
-  // Start the NestJS application
+
   await app.listen(process.env.PORT, () => {
     console.log(`Application is running on: http://localhost:${process.env.PORT}`);
   });
