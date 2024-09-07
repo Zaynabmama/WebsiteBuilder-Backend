0c52ce2bf4e28a156195c32ec4971ebee3543fc5
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 02:09:46 +0300

    Adding helper method to copy base React template

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,8 +1,11 @@
-import { BadRequestException, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { mkdir, copyFile } from 'fs/promises';
+import { join } from 'path';
+import { readdir } from 'fs/promises';
 
 @Injectable()
 export class ProjectService {
@@ -17,12 +20,25 @@ export class ProjectService {
     if (!user) {
         throw new NotFoundException('User not found');
     }
+    const projectName = createProjectDto.name.replace(/\s+/g, '-').toLowerCase();
+    const userFolder = join(__dirname, '..', '..', 'projects', user.name); // User folder path
+    const projectFolder = join(userFolder, projectName); // Project folder path
+
     const existingProject = user.projects.find(
       project => project.name === createProjectDto.name
     );
     if (existingProject) {
       throw new BadRequestException('Project with this name already exists');
     }
+
+   
+    try {
+      await mkdir(projectFolder, { recursive: true });
+    } catch (error) {
+      throw new InternalServerErrorException('Could not create project directory');
+    }
+    await this.copyTemplate(projectFolder);
+    
     const newProject=user.projects.create(createProjectDto as any);
     // const newProject = {
     //   ...createProjectDto,
@@ -33,6 +49,33 @@ export class ProjectService {
     
     return newProject;
   }
+  private async copyTemplate(destination: string): Promise<void> {
+    const templatePath = join(__dirname, '..', '..', 'template');
+    const copyRecursive = async (src: string, dest: string) => {
+      const entries = await readdir(src, { withFileTypes: true });
+      await mkdir(dest, { recursive: true });
+
+      for (const entry of entries) {
+        const srcPath = join(src, entry.name);
+        const destPath = join(dest, entry.name);
+
+        if (entry.isDirectory()) {
+          await copyRecursive(srcPath, destPath);
+        } else {
+          await copyFile(srcPath, destPath);
+        }
+      }
+    };
+
+    try {
+      await copyRecursive(templatePath, destination);
+    } catch (error) {
+      console.error('Error copying template:', error);
+      throw new InternalServerErrorException('Failed to copy React template');
+    }
+  }
+
+
   async listProjects(userId: string): Promise<any[]> {
     const user = await this.userModel.findById(userId);
 
