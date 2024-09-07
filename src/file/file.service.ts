f5ab14a3748a63e4f5330a5708dd39138b739ec7
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 02:51:08 +0300

    editing file service

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,35 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { writeFile, mkdir } from 'fs/promises';
+import path, { dirname } from 'path';
 
 @Injectable()
 export class FileService {
-  private baseUploadPath = path.resolve(__dirname, '..', '..', 'uploads');
+  
+  async uploadJsxFile(fileContent: string, filePath: string): Promise<void> {
+    try {
+      
+      await this.ensureDirectoryExists(dirname(filePath));
 
-  async uploadJsxFile(content: string, filePath: string): Promise<void> {
-    let fullPath = filePath;
-
-
-    if (!path.isAbsolute(filePath)) {
-      fullPath = path.join(this.baseUploadPath, filePath);
-    }
-
-    await fs.promises.mkdir(path.dirname(fullPath), { recursive: true });
-
-   
-    await fs.promises.writeFile(fullPath, content, 'utf8');
-  }
-
-  async deleteJsxFile(filePath: string): Promise<void> {
-    let fullPath = filePath;
-
-
-    if (!path.isAbsolute(filePath)) {
-      fullPath = path.join(this.baseUploadPath, filePath);
-    }
-
-    if (fs.existsSync(fullPath)) {
-      fs.unlinkSync(fullPath);
+      console.log(`Saving JSX file to: ${filePath}`);
+      await writeFile(filePath, fileContent);
+      console.log('JSX file saved successfully!');
+    } catch (err) {
+      console.error(`Failed to write file: ${err.message}`);
+      throw new InternalServerErrorException('Error writing the JSX file');
     }
   }
+  private async ensureDirectoryExists(directoryPath: string): Promise<void> {
+    try {
+      await mkdir(directoryPath, { recursive: true });
+    } catch (err) {
+      throw new InternalServerErrorException(`Error creating directory: ${err.message}`);
+    }
+  }
+
+  // async deleteJsxFile(filePath: string): Promise<void> {
+  //   let fullPath = filePath;
+
+
+  //   if (!path.isAbsolute(filePath)) {
+  //     fullPath = path.join(this.baseUploadPath, filePath);
+  //   }
+
+  //   if (fs.existsSync(fullPath)) {
+  //     fs.unlinkSync(fullPath);
+  //   }
+  
 }
