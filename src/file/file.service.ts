500917297fe81364f881a579f0ba7481d5d11841
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 18:09:49 +0300

    solving file issues

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,39 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { writeFile, mkdir, copyFile } from 'fs';
-import { join, dirname } from 'path';
 import { promises as fsPromises } from 'fs';
+import { mkdir, copyFile } from 'fs/promises';
+import { join, dirname } from 'path';
 
 @Injectable()
 export class FileService {
-   async ensureDirectoryExists(filePath: string): Promise<void> {
+   async ensureDirectoryExists(filePath: string): Promise<any> {
     const dir = dirname(filePath);
-    return new Promise((resolve, reject) => {
-      mkdir(dir, { recursive: true }, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+    return fsPromises.mkdir(dir, { recursive: true });
+   }
+
+   async copyTemplateToProject(projectDir: string): Promise<void> {
+    const templateDir = join(__dirname, '..', '..', 'template');
+
+    try {
+      console.log(`Copying template from ${templateDir} to ${projectDir}`);
+     
+      await fsPromises.cp(templateDir, projectDir, { recursive: true });
+      console.log('Template copied successfully.');
+    } catch (error) {
+      console.error('Error copying template:', error);
+      throw new Error('Failed to copy project template');
+    }
   }
 
-  async copyTemplateToProject(projectDir: string): Promise<void> {
-    const templateDir = join(__dirname, '..', '..', 'template');
-    
-    await fsPromises.cp(templateDir, projectDir, { recursive: true });
-  }
   async uploadJsxFile(fileContent: string, filePath: string): Promise<void> {
     await this.ensureDirectoryExists(filePath);
-    return new Promise((resolve, reject) => {
-      writeFile(filePath, fileContent, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+    return fsPromises.writeFile(filePath, fileContent, 'utf8');
   }
 }
 
