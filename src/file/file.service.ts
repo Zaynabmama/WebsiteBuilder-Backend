39e06ffecbbe6e905213fce91052a11940a71324
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 16:06:45 +0300

    editing file service

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,30 +1,41 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { writeFile, mkdir } from 'fs/promises';
-import path, { dirname } from 'path';
+import { Injectable } from '@nestjs/common';
+import { writeFile, mkdir, copyFile } from 'fs';
+import { join, dirname } from 'path';
+import { promises as fsPromises } from 'fs';
 
 @Injectable()
 export class FileService {
-  
-  async uploadJsxFile(fileContent: string, filePath: string): Promise<void> {
-    try {
-      
-      await this.ensureDirectoryExists(dirname(filePath));
+  private async ensureDirectoryExists(filePath: string): Promise<void> {
+    const dir = dirname(filePath);
+    return new Promise((resolve, reject) => {
+      mkdir(dir, { recursive: true }, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
 
-      console.log(`Saving JSX file to: ${filePath}`);
-      await writeFile(filePath, fileContent);
-      console.log('JSX file saved successfully!');
-    } catch (err) {
-      console.error(`Failed to write file: ${err.message}`);
-      throw new InternalServerErrorException('Error writing the JSX file');
-    }
+  async copyTemplateToProject(projectDir: string): Promise<void> {
+    const templateDir = join(__dirname, '..', '..', 'template');
+    
+    await fsPromises.cp(templateDir, projectDir, { recursive: true });
   }
-  private async ensureDirectoryExists(directoryPath: string): Promise<void> {
-    try {
-      await mkdir(directoryPath, { recursive: true });
-    } catch (err) {
-      throw new InternalServerErrorException(`Error creating directory: ${err.message}`);
-    }
+  async uploadJsxFile(fileContent: string, filePath: string): Promise<void> {
+    await this.ensureDirectoryExists(filePath);
+    return new Promise((resolve, reject) => {
+      writeFile(filePath, fileContent, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
+}
 
   // async deleteJsxFile(filePath: string): Promise<void> {
   //   let fullPath = filePath;
@@ -38,4 +49,3 @@ export class FileService {
   //     fs.unlinkSync(fullPath);
   //   }
   
-}
