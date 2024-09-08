47908113faa6e0f87a63fec02eb27b4c04b9e09b
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 22:20:59 +0300

    fixing styles

--- src/jsxgenerate/jsxgenerate.service.ts
+++ src/jsxgenerate/jsxgenerate.service.ts
@@ -61,24 +61,17 @@ export class JSXGeneratorService {
   private generateStyle(properties: any): string {
     const styleProperties: any = {};
     
-    if (properties.color) {
-      styleProperties.color = properties.color;
-    }
-    if (properties.backgroundColor) {
-      styleProperties.backgroundColor = properties.backgroundColor;
-    }
-    if (properties.fontSize) {
-      styleProperties.fontSize = properties.fontSize;
-    }
-    if (properties.width) {
-      styleProperties.width = properties.width;
-    }
-    if (properties.height) {
-      styleProperties.height = properties.height;
-    }
-    return JSON.stringify(styleProperties);
-  }
-}
+    if (properties.color) styleProperties.color = properties.color;
+    if (properties.backgroundColor) styleProperties.backgroundColor = properties.backgroundColor;
+    if (properties.fontSize) styleProperties.fontSize = properties.fontSize;
+    if (properties.width) styleProperties.width = properties.width;
+    if (properties.height) styleProperties.height = properties.height;
+    if (properties.padding) styleProperties.padding = properties.padding;
+    if (properties.justifyContent) styleProperties.justifyContent = properties.justifyContent;
+    if (properties.alignItems) styleProperties.alignItems = properties.alignItems;
+    
+    return JSON.stringify(styleProperties).replace(/"([^"]+)":/g, '$1:');
+  }}
 
 
 
