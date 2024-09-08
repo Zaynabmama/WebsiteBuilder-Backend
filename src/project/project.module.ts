e42c9e7f57d6c20a2138b4357edb08585b96875a
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-08 16:03:04 +0300

    updating .gitignore to ignore /projects directory and refactored ProjectService to use fs-extra and copy project template

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
 import { UserModule } from 'src/user/user.module';
+import { FileModule } from 'src/file/file.module';
 
 @Module({
   imports: [
-    UserModule,
+    UserModule,FileModule
   ],
   providers: [ProjectService],
   controllers: [ProjectController]
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -3,16 +3,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema/user.schema';
 import { CreateProjectDto } from './dto/create-project.dto';
-import { mkdir, copyFile } from 'fs/promises';
+import { FileService } from 'src/file/file.service';
 import { join } from 'path';
-import { readdir } from 'fs/promises';
 
 @Injectable()
 export class ProjectService {
   constructor(
     
     @InjectModel(User.name) private userModel: Model<User>,// injects the Mongoose model associated with the User schema to interact with the User collection in MongoDB
-  ) {}
+    private readonly fileService: FileService,
+    ) {}
 
   async createProject(userId: string, createProjectDto: CreateProjectDto): Promise<any> {
     const user = await this.userModel.findById(userId);
@@ -21,8 +21,8 @@ export class ProjectService {
         throw new NotFoundException('User not found');
     }
     const projectName = createProjectDto.name.replace(/\s+/g, '-').toLowerCase();
-    const userFolder = join(__dirname, '..', '..', 'projects', user.name); // User folder path
-    const projectFolder = join(userFolder, projectName); // Project folder path
+    const projectDir = join(__dirname, '..', '..', 'projects', projectName);
+
 
     const existingProject = user.projects.find(
       project => project.name === createProjectDto.name
@@ -31,13 +31,6 @@ export class ProjectService {
       throw new BadRequestException('Project with this name already exists');
     }
 
-   
-    try {
-      await mkdir(projectFolder, { recursive: true });
-    } catch (error) {
-      throw new InternalServerErrorException('Could not create project directory');
-    }
-    await this.copyTemplate(projectFolder);
     
     const newProject=user.projects.create(createProjectDto as any);
     // const newProject = {
@@ -46,34 +39,11 @@ export class ProjectService {
     // };
     user.projects.push(newProject);
     await user.save();
+    await this.fileService.copyTemplateToProject(projectDir);
     
     return newProject;
   }
-  private async copyTemplate(destination: string): Promise<void> {
-    const templatePath = join(__dirname, '..', '..', 'template');
-    const copyRecursive = async (src: string, dest: string) => {
-      const entries = await readdir(src, { withFileTypes: true });
-      await mkdir(dest, { recursive: true });
-
-      for (const entry of entries) {
-        const srcPath = join(src, entry.name);
-        const destPath = join(dest, entry.name);
-
-        if (entry.isDirectory()) {
-          await copyRecursive(srcPath, destPath);
-        } else {
-          await copyFile(srcPath, destPath);
-        }
-      }
-    };
-
-    try {
-      await copyRecursive(templatePath, destination);
-    } catch (error) {
-      console.error('Error copying template:', error);
-      throw new InternalServerErrorException('Failed to copy React template');
-    }
-  }
+  
 
 
   async listProjects(userId: string): Promise<any[]> {
