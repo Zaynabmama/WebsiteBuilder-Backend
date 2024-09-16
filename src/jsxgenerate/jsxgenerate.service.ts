b137423b2986cb7206433294ad31acb61b84a5ff
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-17 02:48:58 +0300

    UPDATING JSX GENERATE

--- src/jsxgenerate/jsxgenerate.service.ts
+++ src/jsxgenerate/jsxgenerate.service.ts
@@ -5,78 +5,63 @@ import { FileService } from '../file/file.service';
 export class JSXGeneratorService {
   constructor(private readonly fileService: FileService) {}
 
-  async generateAndSaveJsxFile(components: any[], jsxFilePath: string , pageName: string): Promise<void> {
-    const jsxContent = this.generateJsxContent(components ,pageName);
+  async generateAndSaveJsxFile(components: any[], jsxFilePath: string, pageName: string): Promise<void> {
+    const jsxContent = this.generateJsxContent(components, pageName);
     await this.fileService.uploadJsxFile(jsxContent, jsxFilePath);
   }
 
-   generateJsxContent(components: any[] , pageName: string): string {
-    const usedComponents = this.extractUsedComponents(components);
-    const componentBody = components.map(comp => this.generateComponent(comp)).join('\n');
-    
+  generateJsxContent(components: any[], pageName: string): string {
+    const componentsJsx = components.map(component => this.generateComponentJsx(component)).join('\n\n');
     return `
       import React from 'react';
-    
-      const ${pageName} = () => (
-        <div>
-          ${componentBody}
-        </div>
-      );
+
+      const ${pageName} = () => {
+        return (
+          <div>
+            ${componentsJsx}
+          </div>
+        );
+      }
 
       export default ${pageName};
-    `.trim();
-  }
-  private extractUsedComponents(components: any[]): string[] {
-    const componentNames = components.map(comp => comp.type);
-    return Array.from(new Set(componentNames));
+    `;
   }
 
-  private generateComponent(comp: any): string {
-    const props = this.generateProps(comp.properties);
-    const text = comp.properties.text || '';
-    const selfClosingTags = ['img', 'input', 'br', 'hr'];
-    const style = this.generateStyle(comp.properties); 
-    if (selfClosingTags.includes(comp.type.toLowerCase())) {
-      // return `<${comp.type}${props ? ' ' + props : ''} />`;
-      return `<${comp.type} style={${style}} />`;
+  generateComponentJsx(component: any): string {
+    const { type, properties } = component;
+
+    switch (type) {
+      case 'navbar':
+        return this.generateNavbarJsx(properties);
+      default:
+        return '<div>Unknown Component</div>';
     }
-
-    // return `<${comp.type}${props ? ' ' + props : ''}>${text}</${comp.type}>`;
-    return `<${comp.type} style={${style}}>${text}</${comp.type}>`;
   }
 
-
-  //   return text
-  //     ? `<${comp.type}${props ? ' ' + props : ''}>${text}</${comp.type}>`
-  //     : `<${comp.type}${props ? ' ' + props : ''} />`;
-  // }
-
-  private generateProps(properties: any): string {
-    return Object.keys(properties || {})
-      .filter(prop => prop !== 'text')
-      .map(prop => `${prop}="${properties[prop]}"`)
-      .join(' ');
-  }
-  private generateStyle(properties: any): string {
-    const styleProperties: any = {};
-    if (properties.color) styleProperties.color = `"${properties.color}"`;
-    if (properties.backgroundColor) styleProperties.backgroundColor = `"${properties.backgroundColor}"`;
-    if (properties.fontSize) styleProperties.fontSize = `"${properties.fontSize}"`;
-    if (properties.width) styleProperties.width = `"${properties.width}"`;
-    if (properties.height) styleProperties.height = `"${properties.height}"`;
-    if (properties.padding) styleProperties.padding = `"${properties.padding}"`;
-    if (properties.justifyContent) styleProperties.justifyContent = `"${properties.justifyContent}"`;
-    if (properties.alignItems) styleProperties.alignItems = `"${properties.alignItems}"`;
-
+  generateNavbarJsx(properties: any): string {
+    const { backgroundColor, color, logo, links, flexDirection, justifyContent, alignItems } = properties;
     
-    const styleObject = Object.entries(styleProperties)
-      .map(([key, value]) => `${key}: ${value}`)
-      .join(', ');
+    const linksJsx = links
+      .map((link: any) => `<a href="${link.href}" style={{ color: '${color}' }}>${link.name}</a>`)
+      .join('\n');
 
-    return `{ ${styleObject} }`; 
+    return `
+      <nav style={{
+        backgroundColor: '${backgroundColor}', 
+        color: '${color}', 
+        display: 'flex', 
+        flexDirection: '${flexDirection}', 
+        justifyContent: '${justifyContent}', 
+        alignItems: '${alignItems}', 
+        padding: '10px'
+      }}>
+        <div style={{ fontWeight: 'bold', fontSize: '20px', marginRight: 'auto' }}>
+          ${logo}
+        </div>
+        <div style={{ display: 'flex', gap: '15px' }}>
+          ${linksJsx}
+        </div>
+      </nav>
+    `;
   }
 }
-
-
-
-    
