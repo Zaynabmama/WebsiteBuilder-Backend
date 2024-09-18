97bb469fc5a3ecd28c441034744d21221b9e321f
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-18 21:54:02 +0300

    updating controller

--- src/deployment/deployment.controller.ts
+++ src/deployment/deployment.controller.ts
@@ -7,6 +7,8 @@ import {
   UseGuards,
   HttpException,
   HttpStatus,
+  Body,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { DeploymentService } from './deployment.service';
 import { Request } from 'express';
@@ -14,58 +16,25 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @UseGuards(JwtAuthGuard)
 @Controller('deployment')
+
 export class DeploymentController {
   constructor(private readonly deploymentService: DeploymentService) {}
 
-  
-  @Post('create-site/:projectId')
-  async createSite(
+
+  @Post(':projectId')
+  async deployProject(
     @Req() req,
     @Param('projectId') projectId: string,
-  ): Promise<string> {
-    const userId = req.user.userId; 
-    return await this.deploymentService.createSiteForUser(userId, projectId);
-  }
-
-//   @Post('deploy/:userId/:projectName')
-// async deploy(@Param('userId') userId: string, @Param('projectName') projectName: string) {
-//   try {
-//     await this.deploymentService.deployProject(userId, projectName);
-//     return { message: 'Deployment successful' };
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-@Post('deploy/:userId/:projectId')
-async deploy(
-  @Req()req,
-  @Param('projectId') projectId: string) {
-  try {
-    const userId = req.user.userId; 
-    await this.deploymentService.deployProject(userId, projectId);
-    return { message: 'Deployment successful' };
-  } catch (error) {
-    throw new HttpException(
-      { message: 'Deployment failed', error: error.message },
-      HttpStatus.INTERNAL_SERVER_ERROR,
-    );
+    @Body('projectname') projectname: string,
+  ): Promise<{ liveUrl: string }> {
+    try {
+      
+      const userId = req.user.userId;
+      const liveUrl = await this.deploymentService.oneClickDeploy(userId,projectname, projectId);
+      return { liveUrl }; 
+    } catch (error) {
+      console.error('Deployment failed:', error.message);
+      throw new InternalServerErrorException('Failed to deploy the project');
+    }
   }
 }
-}
-
-  
-  // @Get('status/:siteId')
-  // async checkDeploymentStatus(
-  //   @Param('siteId') siteId: string,
-  // ): Promise<any> {
-  //   return await this.deploymentService.checkDeploymentStatus(siteId);
-  // }
-
-  // @Get('jsx-files/:projectId')
-  // async getJsxFiles(
-  //   @Req() req,
-  //   @Param('projectId') projectId: string,
-  // ): Promise<string[]> {
-  //   const userId = req.user.userId; 
-  //   return await this.deploymentService.getJsxFilesForProject(userId, projectId);
-  // }
