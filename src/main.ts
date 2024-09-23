c62974d672f73519172f063755cb7e0736b3be0d
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-09-23 03:52:20 +0300

    installing swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,13 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+  .setTitle('API Documentation')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
 
 
   await app.listen(process.env.PORT, () => {
